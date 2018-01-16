8fcb3ad47254c9f4e144da788664a36541bd1712
PatrickYang93 <qyang21@hawk.iit.edu>
2018-01-16 01:54:16 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function getCurrentLocation() {
 */
 
 function setLocation() {
-    $.getJSON('https://data.cityofchicago.org/resource/d62x-nvdr.json', function (data) {
+    $.getJSON('https://data.cityofchicago.org/resource/6zsd-86xi.json', function (data) {
         navigator.geolocation.watchPosition(function (position) {
 
             var pos = {
